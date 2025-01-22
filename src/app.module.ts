b84fd62d0c899cd336910df9138c097146e1e4bf
nazi <nazipaeez777@gmail.com>
2025-01-22 23:27:17 +0330

    Connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,22 @@ import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { TaskAssignmentsModule } from './task-assignments/task-assignments.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
     UsersModule,
     ProjectsModule,
     TasksModule,
