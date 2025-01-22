97ef5bf46d810d34cce72cf0ac92bfa77861f5be
nazi <nazipaeez777@gmail.com>
2025-01-22 23:14:00 +0330

    Config env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { TasksModule } from './tasks/tasks.module';
 import { TaskAssignmentsModule } from './task-assignments/task-assignments.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, ProjectsModule, TasksModule, TaskAssignmentsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    
+    UsersModule,
+    ProjectsModule,
+    TasksModule,
+    TaskAssignmentsModule,
+  ],
 })
 export class AppModule {}
