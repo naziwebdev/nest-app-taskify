244603d7d9cf12e4cff12851fd507401f4850c31
nazi <nazipaeez777@gmail.com>
2025-01-24 22:59:39 +0330

    Develope register service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Res, HttpStatus } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Response } from 'express';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('/register')
+  async register(@Body() body: CreateUserDto, @Res() res: Response) {
+    const newUser = await this.usersService.register(body);
+
+    return res.status(HttpStatus.CREATED).json({
+      data: newUser,
+      statusCode: HttpStatus.CREATED,
+      message: 'user created successfully',
+    });
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  ConflictException,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { RoleUserEnum } from './enums/roleUserEnum';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly saltRounds = 10;
+
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  private async checkUserExist(userData: CreateUserDto) {
+    const user = await this.usersRepository.findOne({
+      where: [{ email: userData.email }, { phone: userData.phone }],
+    });
+
+    return user;
+  }
+
+  private async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt(this.saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+  }
+
+  private async createUser(
+    userData: CreateUserDto,
+    hashedPassword: string,
+    usersCount,
+  ) {
+    const newUser = await this.usersRepository.create({
+      ...userData,
+      password: hashedPassword,
+      role: usersCount > 0 ? RoleUserEnum.USER : RoleUserEnum.ADMIN,
+    });
+
+    return newUser;
+  }
+
+  async register(userData: CreateUserDto) {
+    try {
+      const user = await this.checkUserExist(userData);
+      if (user) {
+        throw new ConflictException('user already registered');
+      }
+
+      const usersCount = await this.usersRepository.count();
+
+      const hashedPassword = await this.hashPassword(userData.password);
+
+      const newUser = await this.createUser(
+        userData,
+        hashedPassword,
+        usersCount,
+      );
+
+      return this.usersRepository.save(newUser);
+    } catch (error) {
+      throw new InternalServerErrorException('Error creating user');
+    }
+  }
+}
