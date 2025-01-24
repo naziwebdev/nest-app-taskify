66be2ef19a58b50905308de14d382bf526b909ad
nazi <nazipaeez777@gmail.com>
2025-01-24 23:13:49 +0330

    Config session & set session at register

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api/v1');
+  app.use(
+    cookieSession({
+      name: 'session',
+      keys: [process.env.SESSION_KEY],
+      maxAge: 24 * 60 * 60 * 1000,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,21 @@ import { Body, Controller, Post, Res, HttpStatus } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { Response } from 'express';
+import { Session } from '@nestjs/common';
 
 @Controller('auth')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('/register')
-  async register(@Body() body: CreateUserDto, @Res() res: Response) {
+  async register(
+    @Body() body: CreateUserDto,
+    @Session() session: any,
+    @Res() res: Response,
+  ) {
     const newUser = await this.usersService.register(body);
+
+    session.userId = newUser.id;
 
     return res.status(HttpStatus.CREATED).json({
       data: newUser,
