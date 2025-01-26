c0d327549aa9950d6265058b1f94d741b3e935b5
nazi <nazipaeez777@gmail.com>
2025-01-26 13:17:03 +0330

    Create task routing

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  Res,
+  HttpStatus,
+  UseGuards,
+  Body,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { UpdateTaskDto } from './dtos/update-task.dto';
+import { UpdateTaskStatusDto } from './dtos/update-task-status.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/decorators/current-user.decorator';
+import { Response } from 'express';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  async create(@Body() body: CreateTaskDto, @Res() res: Response) {}
+
+  @Get('/:project_id/tasks')
+  @UseGuards(AuthGuard)
+  async getAll(@Param('project_id') projectId: string, @Res() res: Response) {}
+
+  @Get('/:task_id')
+  @UseGuards(AuthGuard)
+  async getOne(@Param('task_id') id: string, @Res() res: Response) {}
+
+  @Put('/:task_id')
+  @UseGuards(AuthGuard)
+  async update(
+    @Param('task_id') id: string,
+    @Body() body: UpdateTaskDto,
+    @Res() res: Response,
+  ) {}
+
+  @Patch('/:task_id/status')
+  @UseGuards(AuthGuard)
+  async updateStatusTask(
+    @Param('task_id') id: string,
+    @Body() body: UpdateTaskStatusDto,
+    @Res() res: Response,
+  ) {}
+
+  @Delete('/:task_id')
+  @UseGuards(AuthGuard)
+  async remove(@Param('task_id') id: string, @Res() res: Response) {}
+}
